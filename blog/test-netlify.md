8613c3cd6ec716370a9e1ea05f4fbc2dfa7ff533
yordanatanasovupnetix <yordan.atanasov@upnetix.com>
2019-05-22 14:03:01 +0300

    changed folder

--- blog/test-netlify.md
+++ blog/test-netlify.md
@@ -1,5 +1,5 @@
 ---
-path: dakata
+path: /dakata
 date: 2019-05-22T10:33:49.449Z
 title: 'Test netlify '
 ---
--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
         {
             resolve: `gatsby-source-filesystem`,
             options: {
-                path: `${__dirname}/markdowns`,
+                path: `${__dirname}/blog`,
                 name: "markdown-pages",
             },
         },
